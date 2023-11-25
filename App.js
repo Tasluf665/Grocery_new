548736374e5964c2a524c336f77084f3d991c1a6
Tasluf665 <tasluf15-12089@diu.edu.bd>
2023-11-25 18:52:49 +0600

    Font has been added

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import customeFonts from "./constant/customeFonts";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Gilroy_ExtraBold: require("./assets/fonts/Gilroy/Gilroy_ExtraBold.otf"),
+    Gilroy_Light: require("./assets/fonts/Gilroy/Gilroy_Light.otf"),
+    Lato_Black: require("./assets/fonts/Lato/Lato-Black.ttf"),
+    Lato_BlackItalic: require("./assets/fonts/Lato/Lato-BlackItalic.ttf"),
+    Lato_Bold: require("./assets/fonts/Lato/Lato-Bold.ttf"),
+    Lato_BoldItalic: require("./assets/fonts/Lato/Lato-BoldItalic.ttf"),
+    Lato_Italic: require("./assets/fonts/Lato/Lato-Italic.ttf"),
+    Lato_Light: require("./assets/fonts/Lato/Lato-Light.ttf"),
+    Lato_LightItalic: require("./assets/fonts/Lato/Lato-LightItalic.ttf"),
+    Lato_Regular: require("./assets/fonts/Lato/Lato-Regular.ttf"),
+    Lato_Thin: require("./assets/fonts/Lato/Lato-Thin.ttf"),
+    Lato_ThinItalic: require("./assets/fonts/Lato/Lato-ThinItalic.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: customeFonts.Gilroy_ExtraBold }}>
+        Gilroy_ExtraBold
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
